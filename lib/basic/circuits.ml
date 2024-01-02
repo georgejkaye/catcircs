b597c251b4023155130d84d2805bf30921e6b177
George Kaye <georgejkaye@gmail.com>
2024-01-02 17:26:33 +0000

    [feat] Add test function to circuits

--- lib/basic/circuits.ml
+++ lib/basic/circuits.ml
@@ -12,6 +12,7 @@ type 'a circuit = {
 module type VCirc = sig
   type v
 
+  val test : v circuit -> v io_cmp list
   val compare : v circuit -> v circuit -> v io_cmp list
   val string_of_outputs : v circuit -> string
   val string_of_comparison : v circuit -> v circuit -> string
@@ -25,6 +26,12 @@ module ExtendCircuit (V : Value) : VCirc with type v := V.v = struct
   module VString = ExtendString (V)
   module VEnum = ExtendEnum (V)
   module VInOut = ExtendInOut (V)
+
+  let test c1 =
+    VEnum.enumerate_inputs c1.arity
+    |> List.map ~f:(fun ss ->
+           let out = c1.fn ss in
+           { input = ss; outputs = [| out |] })
 
   let compare c1 c2 =
     VEnum.enumerate_inputs c1.arity
