80f1c8009aa08ced013909a87250dea08eb9c5ab
George Kaye <georgejkaye@gmail.com>
2024-01-12 00:22:03 +0000

    [feat] Add function for making belnap expressions

--- lib/concrete/belnap.ml
+++ lib/concrete/belnap.ml
@@ -169,3 +169,97 @@ let exp_or_fn xs =
   let f_or = or_fn xs.(0) xs.(4) in
   let t_or = or_fn xs.(3) xs.(7) in
   reexplode f_or t_or
+
+module FalsyMonotoneBoolSim :
+  Booleans.MonotoneBoolSim with type v = belnap and type p = gate = struct
+  type v = belnap
+  type p = gate
+
+  let high = False
+  let low = Bottom
+  let and_op = Or
+  let or_op = And
+end
+
+module FalsyMonotoneBoolMaker =
+  Booleans.ExtendMonotoneBoolMaker (FalsyMonotoneBoolSim) (BelnapValue)
+
+module TruthyMonotoneBoolSim :
+  Booleans.MonotoneBoolSim with type v = belnap and type p = gate = struct
+  type v = belnap
+  type p = gate
+
+  let high = True
+  let low = Bottom
+  let and_op = And
+  let or_op = Or
+end
+
+module TruthyMonotoneBoolMaker =
+  Booleans.ExtendMonotoneBoolMaker (TruthyMonotoneBoolSim) (BelnapValue)
+
+type falsy = FBottom | FFalse
+type truthy = TBottom | TTrue
+
+let falsy_to_belnap x y =
+  match (x, y) with
+  | Bottom, Bottom -> Bottom
+  | Bottom, False -> True
+  | False, Bottom -> False
+  | False, False -> Top
+  | _ -> failwith "not falsy"
+
+let belnap_to_falsy = function
+  | Bottom -> Bottom
+  | True -> Bottom
+  | False -> False
+  | Top -> False
+
+let truthy_to_belnap x y =
+  match (x, y) with
+  | Bottom, Bottom -> Bottom
+  | Bottom, True -> True
+  | True, Bottom -> Bottom
+  | True, True -> Top
+  | _ -> failwith "not truthy"
+
+let belnap_to_truthy = function
+  | Bottom -> Bottom
+  | True -> True
+  | False -> Bottom
+  | Top -> True
+
+let falsy_var_exp i =
+  if i mod 2 == 0 then Op (And, [| Constant Bottom; Variable (i / 2) |], 0)
+  else Op (Not, [| Op (Or, [| Constant Bottom; Variable (i / 2) |], 0) |], 0)
+
+let truthy_var_exp i =
+  if i mod 2 == 0 then
+    Op (Not, [| Op (And, [| Constant Bottom; Variable (i / 2) |], 0) |], 0)
+  else Op (Or, [| Constant Bottom; Variable (i / 2) |], 0)
+
+let get_exp_from_circ circ =
+  let fn = get_value_fn circ in
+  let get_fn to_belnap from_belnap xs =
+    let belnap_inputs =
+      Array.init
+        (Array.length xs / 2)
+        (fun i ->
+          let base = i * 2 in
+          to_belnap xs.(base) xs.(base + 1))
+    in
+    let belnap_outputs = fn belnap_inputs in
+    let belnap_output = belnap_outputs.(0) in
+    from_belnap belnap_output
+  in
+  let falsy = get_fn falsy_to_belnap belnap_to_falsy in
+  let truthy = get_fn truthy_to_belnap belnap_to_truthy in
+  let falsy_exp =
+    FalsyMonotoneBoolMaker.get_exp_for_function falsy_var_exp falsy
+      (List.length circ.arity * 2)
+  in
+  let truthy_exp =
+    TruthyMonotoneBoolMaker.get_exp_for_function truthy_var_exp truthy
+      (List.length circ.arity * 2)
+  in
+  Op (Join, [| falsy_exp; truthy_exp |], 0)
