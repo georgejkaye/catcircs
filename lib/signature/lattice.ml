1e46a9b1f5a82172186a6960a3eb3770f2eaca67
George Kaye <georgejkaye@gmail.com>
2024-02-28 16:37:33 +0000

    [fix] Make extendlattice functor work properly

--- lib/signature/lattice.ml
+++ lib/signature/lattice.ml
@@ -11,28 +11,54 @@ module type LatticeDerived = sig
   type v
 
   val leq : v -> v -> bool
+  val leq_signal : v signal -> v signal -> bool
+  val leq_inputs : v signal array -> v signal array -> bool
   val geq : v -> v -> bool
+  val geq_signal : v signal -> v signal -> bool
+  val geq_inputs : v signal array -> v signal array -> bool
   val bot : v
   val top : v
 end
 
-module ExtendLattice
-    (V : Value)
-    (L : Lattice with type v = V.v)
-    (LD : LatticeDerived with type v = V.v) =
-struct
+module ExtendLattice (V : Value) (L : Lattice with type v = V.v) :
+  LatticeDerived with type v := V.v = struct
   let leq a b = Core.phys_equal b (L.join a b)
+
+  let leq_signal xs ys =
+    Array.fold_left
+      (fun acc cur -> acc && cur)
+      true
+      (Array.map2 (fun x y -> leq x y) xs.values ys.values)
+
+  let leq_inputs xss yss =
+    Array.fold_left
+      (fun acc cur -> acc && cur)
+      true
+      (Array.map2 (fun xs ys -> leq_signal xs ys) xss yss)
+
   let geq a b = (not (leq a b)) && not (Core.phys_equal a b)
+
+  let geq_signal xs ys =
+    Array.fold_left
+      (fun acc cur -> acc && cur)
+      true
+      (Array.map2 (fun x y -> geq x y) xs.values ys.values)
+
+  let geq_inputs xss yss =
+    Array.fold_left
+      (fun acc cur -> acc && cur)
+      true
+      (Array.map2 (fun xs ys -> geq_signal xs ys) xss yss)
 
   let bot =
     let values = V.all_of_v in
     List.fold_left
-      (fun acc cur -> if LD.leq acc cur then acc else cur)
+      (fun acc cur -> if leq acc cur then acc else cur)
       (List.hd values) (List.tl values)
 
   let top =
     let values = V.all_of_v in
     List.fold_left
-      (fun acc cur -> if LD.geq acc cur then acc else cur)
+      (fun acc cur -> if geq acc cur then acc else cur)
       (List.hd values) (List.tl values)
 end
